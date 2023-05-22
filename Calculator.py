3e176251ec5b1772a268bffa98c6082a9a86ff4b
Bhea <bheaisipnobleza@gmail.com>
2023-05-22 12:07:46 +0800

    added functions for each operation

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,18 @@
 # Create a program that will calculate using four operation such as additon, subtraction, multiplication, division
+class simpleCalculator:
     # If addition
+    def add(self, integer1, integer2):
+        result = integer1 + integer2
+        return result
     # If subtraction
+    def sub(self, integer1, integer2):
+        result = integer1 - integer2
+        return result
     # If multiplication
+    def mult(self, integer1, integer2):
+        result = integer1 * integer2
+        return result
     # If division
+    def div(self, integer1, integer2):
+        result = integer1 / integer2
+        return result
