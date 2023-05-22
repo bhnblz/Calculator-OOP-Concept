a36bb7da380766d0e2c4fa327a240ee9635813db
Bhea <bheaisipnobleza@gmail.com>
2023-05-22 11:57:39 +0800

    pseudocode for Calculator file

---
+++ Calculator.py
@@ -0,0 +1,5 @@
+# Create a program that will calculate using four operation such as additon, subtraction, multiplication, division
+    # If addition
+    # If subtraction
+    # If multiplication
+    # If division
